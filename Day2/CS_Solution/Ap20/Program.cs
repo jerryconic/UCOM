95712d0ffe50c635ded14a719cfdc248d9998bec
Jerry Huang <85143679+jerryconic@users.noreply.github.com>
2025-10-09 16:52:59 +0800

    Final

---
+++ Day2/CS_Solution/Ap20/Program.cs
@@ -0,0 +1,14 @@
+﻿Console.WriteLine("Begin");
+goto label2;
+
+label1:
+Console.WriteLine("Label1");
+goto label3;
+
+label2:
+Console.WriteLine("Label2");
+goto label1;
+
+label3:
+Console.WriteLine("Label3");
+Console.WriteLine("End.");
---
+++ Day2/CS_Solution/Ap21/Program.cs
@@ -0,0 +1,6 @@
+﻿using Ap21;
+
+Person p1 = new Person();
+p1.id = "C001";
+p1.name = "John";
+Console.WriteLine($"Id:{p1.id}, Name:{p1.name}");
