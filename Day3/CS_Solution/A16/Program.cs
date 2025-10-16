eb27ccfc366d03907c03dbbdcb616342831dd74f
jerryconic <85143679+jerryconic@users.noreply.github.com>
2025-10-16 16:54:17 +0800

    Final_Day3

---
+++ Day3/CS_Solution/A16/Program.cs
@@ -0,0 +1,4 @@
+﻿StreamReader rd = new StreamReader(@"D:\temp\test.txt");
+string s = rd.ReadToEnd();
+Console.WriteLine(s);
+rd.Close();
---
+++ Day3/CS_Solution/Ap15/Program.cs
@@ -0,0 +1,10 @@
+﻿StreamWriter wr = new StreamWriter(@"D:\temp\test.txt");
+/*
+StreamWriter wr = new StreamWriter("C:\\temp\\test.txt");
+StreamWriter wr = new StreamWriter("C:/temp/test.txt");
+ */
+
+for (int i = 0; i < 10; i++)
+    wr.WriteLine("Hello world!");
+wr.Close();
+Console.WriteLine("執行完畢");
