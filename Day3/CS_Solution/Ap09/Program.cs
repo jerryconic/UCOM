d4d538d3b2e622d6571cbd1b4d9f20f398a87073
jerryconic <85143679+jerryconic@users.noreply.github.com>
2025-10-16 15:50:59 +0800

    Day3

---
+++ Day3/CS_Solution/Ap09/Program.cs
@@ -0,0 +1,22 @@
+﻿using System.Runtime.CompilerServices;
+
+Person p1 = new Person();
+Person p2 = new Person();
+Person p3 = new Person();
+Console.WriteLine(Person.count);
+
+public class Person
+{
+    private string _id, _name;
+    public static int count=0;
+    public Person()
+    {
+        count++;
+    }
+    public static void ShowCount()
+    {
+        
+        Console.WriteLine(count);
+    }
+
+}
---
+++ Day3/CS_Solution/Ap10/Program.cs
@@ -0,0 +1,25 @@
+﻿Console.WriteLine(Math.PI);
+Console.WriteLine(Math.Sin(Math.PI/2));
+Console.WriteLine(Math.Sin(Math.PI / 4));
+Console.WriteLine(Math.Round(4.5, 0)); //Banker捨入法
+Console.WriteLine(Math.Round(4.5, 0, MidpointRounding.AwayFromZero));
+
+Console.WriteLine(DateTime.Now);
+Console.WriteLine(DateTime.Today.ToString("yyyy/MM/dd"));
+DateTime d1 = DateTime.Now;
+d1 = d1.AddYears(2);
+Console.WriteLine(d1);
+DateTime d2 = DateTime.Now;
+Console.WriteLine((d1-d2).TotalDays);
+
+string s = "ABCD";
+Console.WriteLine(s[0]);
+Console.WriteLine(s.ToLower());
+if(s.StartsWith("A"))
+    Console.WriteLine("Start A");
+else
+    Console.WriteLine("Not");
+
+int n;
+
+Console.WriteLine(s.Length);
